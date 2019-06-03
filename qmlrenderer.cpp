a273dc973ed1839a52db6da0d60be862642ddde5
Akhil K Gangadharan <helloimakhil@gmail.com>
2019-06-03 20:42:22 +0530

    Add QML rendered

---
+++ qmlrenderer.cpp
@@ -0,0 +1,346 @@
+/*
+Copyright (C) 2019  Akhil K Gangadharan <helloimakhil@gmail.com>
+This file is part of Kdenlive. See www.kdenlive.org.
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License as
+published by the Free Software Foundation; either version 2 of
+the License or (at your option) version 3 or any later version
+accepted by the membership of KDE e.V. (or its successor approved
+by the membership of KDE e.V.), which shall act as a proxy
+defined in Section 14 of version 3 of the license.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <QOpenGLContext>
+#include <QOpenGLFunctions>
+#include <QOpenGLFramebufferObject>
+#include <QOpenGLShaderProgram>
+#include <QOpenGLVertexArrayObject>
+#include <QOpenGLBuffer>
+#include <QOpenGLVertexArrayObject>
+#include <QOffscreenSurface>
+#include <QScreen>
+#include <QQmlEngine>
+#include <QQmlComponent>
+#include <QQuickItem>
+#include <QQuickWindow>
+#include <QQuickRenderControl>
+#include <QCoreApplication>
+#include <QEvent>
+#include <QtConcurrent/QtConcurrent>
+#include <memory>
+#include <QDebug>
+
+#include "qmlrenderer.h"
+#include "qmlanimationdriver.h"
+
+QmlRenderer::QmlRenderer(QObject *parent)
+    : QObject(parent)
+    , m_context(nullptr)
+    , m_offscreenSurface(nullptr)
+    , m_renderControl(nullptr)
+    , m_quickWindow(nullptr)
+    , m_qmlEngine(nullptr)
+    , m_qmlComponent(nullptr)
+    , m_rootItem(nullptr)
+    , m_fbo(nullptr)
+    , m_animationDriver(nullptr)
+    , updateRequest(nullptr)
+    , qEvent(nullptr)
+    , m_status(NotRunning)
+
+{
+    QSurfaceFormat format;
+    // Qt Quick may need a depth and stencil buffer. Always make sure these are available.
+    format.setDepthBufferSize(16);
+    format.setStencilBufferSize(8);
+
+    m_context = std::make_unique<QOpenGLContext>();
+    m_context->setFormat(format);
+    m_context->create();
+
+    m_offscreenSurface = std::make_unique<QOffscreenSurface>();
+    m_offscreenSurface->setFormat(m_context->format());
+    m_offscreenSurface->create();
+
+    m_renderControl = std::make_unique<QQuickRenderControl>(this);
+    m_quickWindow = std::make_unique<QQuickWindow>(m_renderControl.get());
+    m_qmlEngine = std::make_unique<QQmlEngine>();
+
+    if (!m_qmlEngine->incubationController())
+        m_qmlEngine->setIncubationController(m_quickWindow->incubationController());
+
+    m_context->makeCurrent(m_offscreenSurface.get());
+    m_renderControl->initialize(m_context.get());
+
+    std::unique_ptr<QEvent> test = std::make_unique<QEvent>(QEvent::UpdateRequest);
+    qEvent.reset(test.get());
+}
+
+void QmlRenderer::renderQml(const QString &qmlFile, const QString &filename, const QString &outputDirectory, const QString &outputFormat, const QSize &size, qreal devicePixelRatio, int durationMs, int fps,  bool checkIfEntire, qint64 frameTime)
+{
+
+    if (m_status != NotRunning)
+        return;
+
+    m_size = size;
+    m_dpr = devicePixelRatio;
+    m_duration = durationMs;
+    m_fps = fps;
+    m_outputName = filename;
+    m_outputDirectory = outputDirectory;
+    m_outputFormat = outputFormat;
+    m_frameTime = frameTime;
+
+   if (!loadQML(qmlFile, size)) {
+       return;
+    }
+
+   m_status = Running;
+   createFbo();
+
+   if (!m_context->makeCurrent(m_offscreenSurface.get()))
+       return;
+
+   // Render each frame of movie
+   m_frames = m_duration / 1000 * m_fps;
+   m_animationDriver = std::make_unique<QmlAnimationDriver>(1000/m_fps);
+   m_animationDriver->install();
+   m_currentFrame = 0;
+   m_futureCounter = 0;
+    m_checkIfEntire = checkIfEntire;
+
+   if(checkIfEntire== true) // render entire qml frames
+        renderEntireQml();
+   else {                               // render only select frame
+       m_selectFrame  =  static_cast<int>(m_frameTime / ((1000/m_fps)));
+       renderOneFrame();
+
+   }
+}
+
+QmlRenderer::~QmlRenderer()
+{
+    m_context->makeCurrent(m_offscreenSurface.get());
+    m_context->doneCurrent();
+}
+
+int QmlRenderer::progress() const
+{
+    return m_progress;
+}
+
+void QmlRenderer::cleanup()
+{
+    m_animationDriver->uninstall();
+    m_animationDriver.reset();
+    destroyFbo();
+}
+
+void QmlRenderer::createFbo()
+{
+    m_fbo = std::make_unique<QOpenGLFramebufferObject>(m_size * m_dpr, QOpenGLFramebufferObject::CombinedDepthStencil);
+    m_quickWindow->setRenderTarget(m_fbo.get());
+}
+
+void QmlRenderer::destroyFbo()
+{
+    m_fbo.reset();
+}
+
+void QmlRenderer::checkComponent()
+{
+     if (m_qmlComponent->isError()) {
+        const QList<QQmlError> errorList = m_qmlComponent->errors();
+        for (const QQmlError &error : errorList)
+            qDebug() << error.url() << error.line() << error;
+    }
+}
+
+bool QmlRenderer::loadQML(const QString &qmlFile, const QSize &size)
+{
+    if (m_qmlComponent != nullptr) {
+        m_qmlComponent.reset();
+    }
+    m_qmlComponent = std::make_unique<QQmlComponent>(m_qmlEngine.get(), QUrl(qmlFile), QQmlComponent::PreferSynchronous);
+
+    checkComponent();
+
+    QQmlEngine::setObjectOwnership(rootObject.get(), QQmlEngine::CppOwnership);
+    rootObject.reset(m_qmlComponent->create());
+
+    checkComponent();
+
+    m_rootItem.reset(qobject_cast<QQuickItem*>(rootObject.get()));
+
+    if (!m_rootItem) {
+        qDebug()<< "run: Not a QQuickItem";
+        rootObject.reset();
+        return false;
+    }
+
+    // The root item is ready. Associate it with the window.
+    m_rootItem->setParentItem(m_quickWindow->contentItem());
+
+    m_rootItem->setWidth(size.width());
+    m_rootItem->setHeight(size.height());
+
+    m_quickWindow->setGeometry(0, 0, size.width(), size.height());
+
+    return true;
+}
+
+void static saveImage(const QImage &image, const QString &outputFile)
+{
+    image.save(outputFile);
+}
+
+void QmlRenderer::futureFinished()
+{
+    m_futureCounter++;
+    if (m_futureCounter == (m_frames - 1)) {
+        m_futures.clear();
+        m_status = NotRunning;
+    }
+}
+
+void QmlRenderer::renderEntireQml()
+{
+    // Render frame
+    m_renderControl->polishItems();
+    m_renderControl->sync();
+    m_renderControl->render();
+
+    m_context->functions()->glFlush();
+
+     m_currentFrame++;
+     m_outputFile =  QString(m_outputDirectory + QDir::separator() + m_outputName + "_" + QString::number(m_currentFrame) + "." + m_outputFormat);
+
+    watcher = std::make_unique<QFutureWatcher<void>>();
+    connect(watcher.get(), SIGNAL(finished()), this, SLOT(futureFinished()));
+    watcher->setFuture(QtConcurrent::run(saveImage, m_fbo->toImage(), m_outputFile));
+    m_futures.append(std::move(watcher));
+
+    //advance animation
+    m_animationDriver->advance();
+
+    if (m_currentFrame < m_frames) {
+        //Schedule the next update
+        updateRequest.reset(qEvent.get());
+        QCoreApplication::postEvent(this, updateRequest.get());
+    } else {
+        //Finished
+        cleanup();
+    }
+
+}
+
+
+bool QmlRenderer::event(QEvent *event)
+{
+    if (event->type() == QEvent::UpdateRequest) {
+        if(m_checkIfEntire)
+            renderEntireQml();
+
+
+        return true;
+    }
+    return QObject::event(event);
+}
+
+bool QmlRenderer::isRunning()
+{
+    return m_status == Running;
+}
+
+void QmlRenderer::renderOneFrame()
+{
+    while(m_animationDriver->elapsed() < m_frameTime) {
+        qDebug() << "ok";
+        m_animationDriver->advance();
+        updateRequest = std::make_unique<QEvent>(QEvent::UpdateRequest);
+        QCoreApplication::postEvent(this, updateRequest.get());
+    }
+    qDebug() << "ok2";
+
+    m_renderControl->polishItems();
+    m_renderControl->sync();
+    m_renderControl->render();
+
+    m_context->functions()->glFlush();
+
+    m_currentFrame =static_cast<int>(m_frameTime / ((1000/m_fps)));
+    m_outputFile =  QString(m_outputDirectory + QDir::separator() + m_outputName + "_" + QString::number(m_currentFrame) + "." + m_outputFormat);
+
+    qDebug() << "you are seeking frame " << m_currentFrame;
+    saveImage(m_fbo->toImage(), m_outputFile);
+
+    cleanup();
+    m_status = NotRunning;
+    qDebug() << "ok3";
+
+}
+void QmlRenderer::renderSelectFrame() // advance animation till required frame
+{
+    m_animationDriver->advance(m_frameTime);
+    QEvent *updateRequest = new QEvent(QEvent::UpdateRequest);
+    QCoreApplication::postEvent(this, updateRequest);
+    // Render frame
+    m_renderControl->polishItems();
+    m_renderControl->sync();
+    m_renderControl->render();
+
+    m_context->functions()->glFlush();
+
+    m_currentFrame =static_cast<int>(m_frameTime / ((1000/m_fps)));
+    m_outputFile =  QString(m_outputDirectory + QDir::separator() + m_outputName + "_" + QString::number(m_currentFrame) + "." + m_outputFormat);
+
+    qDebug() << "you are seeking frame " << m_currentFrame;
+    saveImage(m_fbo->toImage(), m_outputFile);
+
+    cleanup();
+    m_status = NotRunning;
+}
+
+void QmlRenderer::renderSingleFrame()  // render frames till required frame without saving frames
+{
+    // Render frame
+    m_renderControl->polishItems();
+    m_renderControl->sync();
+    m_renderControl->render();
+
+    m_context->functions()->glFlush();
+
+     m_currentFrame++;
+
+     m_outputFile =  QString(m_outputDirectory + QDir::separator() + m_outputName + "_" + QString::number(m_currentFrame) + "." + m_outputFormat);
+
+    watcher = std::make_unique<QFutureWatcher<void>>();
+    connect(watcher.get(), SIGNAL(finished()), this, SLOT(futureFinished()));
+    if(m_currentFrame ==  m_selectFrame) {
+        watcher->setFuture(QtConcurrent::run(saveImage, m_fbo->toImage(), m_outputFile));
+        return;
+    }
+    m_futures.append(std::move(watcher));
+
+    //advance animation
+    m_animationDriver->advance();
+
+    if (m_currentFrame < m_frames) {
+        //Schedule the next update
+       updateRequest = std::make_unique<QEvent>(QEvent::UpdateRequest);
+        QCoreApplication::postEvent(this, updateRequest.get());
+    } else {
+        //Finished
+        cleanup();
+    }
+
+}
---
+++ qmlrenderer.h
@@ -0,0 +1,114 @@
+/*
+Copyright (C) 2019  Akhil K Gangadharan <helloimakhil@gmail.com>
+This file is part of Kdenlive. See www.kdenlive.org.
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License as
+published by the Free Software Foundation; either version 2 of
+the License or (at your option) version 3 or any later version
+accepted by the membership of KDE e.V. (or its successor approved
+by the membership of KDE e.V.), which shall act as a proxy
+defined in Section 14 of version 3 of the license.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef QMLRENDERER_H
+#define QMLRENDERER_H
+
+#include "qmlrenderer_global.h"
+#include <memory>
+
+#include <QObject>
+#include <QSize>
+#include <QString>
+#include <QDir>
+#include <QFuture>
+
+class QOpenGLContext;
+class QOpenGLFramebufferObject;
+class QOffscreenSurface;
+class QQuickRenderControl;
+class QQuickWindow;
+class QQmlEngine;
+class QQmlComponent;
+class QQuickItem;
+class QmlAnimationDriver;
+
+class QMLRENDERERSHARED_EXPORT QmlRenderer: public QObject
+{
+
+    Q_OBJECT
+
+public:
+    QmlRenderer(QObject *parent = 0);
+    ~QmlRenderer();
+
+    void renderQml(const QString &qmlFile, const QString &filename, const QString &outputDirectory, const QString &outputFormat, const QSize &size, qreal devicePixelRatio = 1.0, int durationMs = 1000*5, int fps = 24, bool checkIfEntire=true, qint64 frameTime=0);
+    void renderSelectFrame(); //renders single frame
+    enum Status {
+            NotRunning,
+            Running
+        };
+
+private:
+    void cleanup();
+
+    void createFbo();
+    void destroyFbo();
+    bool loadQML(const QString &qmlFile, const QSize &size);
+    void renderEntireQml();
+    bool isRunning();
+    bool event(QEvent *event);
+    int progress() const;
+    void checkComponent();
+    void renderFrames();
+    void renderSingleFrame();
+    void renderOneFrame();
+    std::unique_ptr<QOpenGLContext> m_context;
+    std::unique_ptr<QOffscreenSurface> m_offscreenSurface;
+    std::unique_ptr<QQuickRenderControl> m_renderControl;
+    std::unique_ptr<QQuickWindow> m_quickWindow;
+    std::unique_ptr<QQmlEngine> m_qmlEngine;
+    std::unique_ptr<QQmlComponent> m_qmlComponent;
+    std::unique_ptr<QQuickItem> m_rootItem;
+    std::unique_ptr<QOpenGLFramebufferObject> m_fbo;
+    std::unique_ptr<QmlAnimationDriver> m_animationDriver;
+    std::unique_ptr<QObject> rootObject;
+    std::unique_ptr<QFutureWatcher<void>> watcher;
+    std::unique_ptr<QEvent> updateRequest;
+    std::unique_ptr<QEvent> qEvent ;
+
+    qreal m_dpr;
+    QSize m_size;
+    Status m_status;
+    qint64 m_selectFrame;
+
+    int m_progress;
+    int m_duration;  // by default = 5 seconds
+    int m_fps; // by default = 24 fps
+    int m_frames;
+    int m_currentFrame;
+    QString m_outputName;
+    QString m_outputFormat;
+    QString m_outputDirectory;
+    QString m_outputFile;
+    qint64 m_frameTime;
+    bool m_checkIfEntire;
+
+    QVector<std::shared_ptr<QFutureWatcher<void>>> m_futures;
+    int m_futureCounter;
+
+private slots:
+    void futureFinished();
+
+
+};
+
+#endif // QMLRENDERER_H
