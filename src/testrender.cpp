3bc86b62402c13ac96f7f1ab59fe2c12c05e80e3
Akhil K Gangadharan <helloimakhil@gmail.com>
2019-06-05 01:37:50 +0530

    Remove unnecessary files

---
+++ src/testrender.cpp
@@ -0,0 +1,6 @@
+#include "testrender.h"
+
+testRender::testRender(QObject *parent) : QObject(parent)
+{
+
+}
---
+++ src/testrender.h
@@ -0,0 +1,17 @@
+#ifndef TESTRENDER_H
+#define TESTRENDER_H
+
+#include <QObject>
+
+class testRender : public QObject
+{
+    Q_OBJECT
+public:
+    explicit testRender(QObject *parent = nullptr);
+
+signals:
+
+public slots:
+};
+
+#endif // TESTRENDER_H
